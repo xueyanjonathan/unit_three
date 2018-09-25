8098e824ef91fd27cdef761a54b9a6d933c57ca2
Jonathan <jonathan@iMac9.local>
2018-09-25 14:14:44 -0400

    Start programming. Learned to use return in the function and write functions to define the side length and color of the plus signs

---
+++ assignment_three.py
@@ -0,0 +1,23 @@
+# Jonathan Lin
+# Assignment Three
+# Use turtle and functions to draw plus signs and fill color in them
+
+import turtle
+
+def length_of_middle_plus_sign():
+    """
+    Ask the user the side length of the plus sign and return it.
+    :return:
+    """
+    side_length = input("What is the side length of the plus sign?")
+    return side_length
+
+def color_of_plus_sign():
+    """
+    Ask the user the color to fill in the plus signs and return it.
+    :return:
+    """
+    color = input("What is the color of the plus signs?")
+    return color
+
+ 
