605b0050da1657206aa0b8d8c3129feca268c09e
Jonathan <jonathan@iMac9.local>
2018-09-26 14:38:39 -0400

    Wrote the function for drawing a plus sign, and successfully ran the main function

--- assignment_three.py
+++ assignment_three.py
@@ -4,13 +4,15 @@
 
 import turtle
 
+
 def length_of_middle_plus_sign():
     """
     Ask the user the side length of the plus sign and return it.
     :return:
     """
     side_length = input("What is the side length of the plus sign?")
-    return side_length
+    return float(side_length)
+
 
 def color_of_plus_sign():
     """
@@ -20,4 +22,36 @@ def color_of_plus_sign():
     color = input("What is the color of the plus signs?")
     return color
 
+
+def draw_a_plus_sign(color, length):
+    """
+    draw a plus sign based on the color and side length given
+    :param color:
+    :param length:
+    :return:
+    """
+    turtle.color(color)
+    turtle.begin_fill()
+    for x in range(4):
+        turtle.forward(length)
+        turtle.left(90)
+        turtle.forward(length)
+        turtle.right(90)
+        turtle.forward(length)
+        turtle.left(90)
+    turtle.end_fill()
+
  
+def main():
+    """
+    Run all the functions
+    :return:
+    """
+    color = color_of_plus_sign()
+    length = length_of_middle_plus_sign()
+    turtle.speed(100)
+    draw_a_plus_sign(color, length)
+    turtle.exitonclick()
+
+
+main()
