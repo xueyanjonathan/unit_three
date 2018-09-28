b3df6d6c62cf7b4dfdc8707fda1b408ccefb0272
Jonathan <jonathan@iMac9.local>
2018-09-28 08:16:48 -0400

    writing document strings

---
+++ d4_unit_three_warmups.py
@@ -0,0 +1,10 @@
+def area_of_rectangle(length, width):
+    """
+    The function uses length and width as parameters to calculate the area of the rectangle by multiplication,
+    and returns the area
+    :param length:the long side of the rectangle
+    :param width:the short side of the rectangle
+    :return: area
+    """
+    area = length * width
+    return area
